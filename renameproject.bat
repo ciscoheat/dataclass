4e2293b44bff049dddbb5134844f2302a0c4642b
Andreas <ciscoheat@gmail.com>
2015-05-01 20:42:54 +0200

    Changed exclude path for renameproject.bat

--- renameproject.bat
+++ renameproject.bat
@@ -1,7 +1,7 @@
 @echo off
 IF [%1] == [] GOTO noargs
-fnr --cl --dir "%CD%" --find "%1" --replace "%2" --fileMask "*.*" --excludeFileMask "*.exe"
-fnr --cl --dir "%CD%\bin" --find "%1" --replace "%2" --fileMask "*.*" --excludeFileMask "*.exe" --excludeFileMask "renameproject.bat"
+fnr --cl --dir "%CD%" --find "%1" --replace "%2" --fileMask "*.*" --excludeFileMask "renameproject.bat"
+fnr --cl --dir "%CD%\bin" --find "%1" --replace "%2" --fileMask "*.*" --excludeFileMask "renameproject.bat"
 rename "%1.hxml" "%2.hxml"
 rename "%1.hxproj" "%2.hxproj"
 rename "%1.sublime-project" "%2.sublime-project"
