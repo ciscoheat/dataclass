78ce00add96cb9864b704b3f4acf24fe26574819
Andreas <ciscoheat@gmail.com>
2015-05-01 18:41:30 +0200

    Added sublime project file.

--- renameproject.bat
+++ renameproject.bat
@@ -4,6 +4,8 @@ fnr --cl --dir "%CD%" --find "%1" --replace "%2" --fileMask "*.*" --excludeFileM
 fnr --cl --dir "%CD%\bin" --find "%1" --replace "%2" --fileMask "*.*" --excludeFileMask "*.exe" --excludeFileMask "renameproject.bat"
 rename "%1.hxml" "%2.hxml"
 rename "%1.hxproj" "%2.hxproj"
+rename "%1.sublime-project" "%2.sublime-project"
+rename "%1.sublime-workspace" "%2.sublime-workspace"
 GOTO end
 :noargs
 echo Usage: renameproject.bat oldname newname
