5fd7d658ce234660a7d9f25a3c257579bcb4dcf8
Andreas <ciscoheat@gmail.com>
2015-05-13 10:52:23 +0200

    Added Contract and DCI to the Main class.

--- src/Main.hx
+++ src/Main.hx
@@ -1,10 +1,32 @@
 package;
 
+import haxecontracts.Contract;
+import haxecontracts.HaxeContracts;
+import haxedci.Context;
 import js.Lib;
 
+using Lambda;
+using StringTools;
+
 class Main 
+implements HaxeContracts implements Context
 {	
 	static function main() {
-		trace("Test");
+		new Main().start();
+	}
+
+	public function new() {
+		Contract.requires(true != false, "Uh-oh.");
+		this.amount = 123;
+	}
+	
+	public function start() {
+		amount.display();
+	}
+
+	@role var amount : Int = {
+		function display() : Void {
+			trace(self);
+		}
 	}
 }
