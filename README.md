a3ebb12b8c9b20a445b4f21e6f8477dbdaa2a3df
Andreas <ciscoheat@gmail.com>
2015-06-03 21:08:39 +0200

    Renaming is now a one-time operation.

--- README.md
+++ README.md
@@ -3,10 +3,10 @@ An empty Haxe/Node.js project, to get started quickly.
 A common first usage setup after cloning the repo:
 
 ```
-renameproject nodeproject my-project-name
 npm install -g nodemon
+renameproject my-project-name
 haxe my-project-name.hxml
 npm start
 ```
 
-`renameproject.bat` which is used to rename the project uses `fnr.exe` which is a rather large .exe file, remove it if you don't need it. A nice contribution would be to make a Haxe file that does the same.
+`renameproject.bat` which is used to rename the project uses `fnr.exe` which is a rather large .exe file, they are both deleted after renaming. A nice contribution would be to make a Haxe file that does the same.
--- renameproject.bat
+++ renameproject.bat
@@ -1,13 +1,14 @@
 @echo off
-IF [%1] == [] GOTO noargs
-fnr --cl --dir "%CD%" --find "%1" --replace "%2" --fileMask "*.*" --excludeFileMask "renameproject.bat"
-fnr --cl --dir "%CD%\bin" --find "%1" --replace "%2" --fileMask "*.*" --excludeFileMask "renameproject.bat"
-rename "%1.hxml" "%2.hxml"
-rename "%1.hxproj" "%2.hxproj"
-rename "%1.sublime-project" "%2.sublime-project"
-rename "%1.sublime-workspace" "%2.sublime-workspace"
+IF [%1] == [] GOTO usage
+fnr --cl --dir "%CD%" --find "nodeproject" --replace "%1" --fileMask "*.*" --excludeFileMask "renameproject.bat"
+fnr --cl --dir "%CD%\bin" --find "nodeproject" --replace "%1" --fileMask "*.*" --excludeFileMask "renameproject.bat"
+rename "nodeproject.hxml" "%1.hxml"
+rename "nodeproject.hxproj" "%1.hxproj"
+rename "nodeproject.sublime-project" "%1.sublime-project"
+rename "nodeproject.sublime-workspace" "%1.sublime-workspace"
+del fnr.exe
+del renameproject.bat
 GOTO end
-:noargs
-echo Usage: renameproject.bat oldname newname
-echo Initial 'oldname' is 'nodeproject'.
+:usage
+echo Usage: renameproject.bat newname
 :end
