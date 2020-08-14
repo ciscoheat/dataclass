505b0d62d6051a927ed6c4f02e044ddfbe1caf45
Andreas <ciscoheat@gmail.com>
2020-08-14 14:11:09 +0200

    Rewritten, works.

--- src/dataclass/DataMap.hx
+++ src/dataclass/DataMap.hx
@@ -13,13 +13,15 @@ using Lambda;
 using StringTools;
 
 @:publicFields @:structInit private class DataClassInfo {    
-    // The identifier used for the field. "person" in person.name
+    // The identifier used for the structure
     final identifier : Expr;
     
-    // All fields in the current object
+    // All fields in the current structure
     final fields : Array<ObjectField>;
-    
-    final currentType : Type;
+
+    // Type as determined by the macro Context.
+    // If instance, 
+    final currentType : Option<Type>;
 }
 #end
 
@@ -36,131 +38,91 @@ class DataMap
 {
     #if macro
 
-    static function createNew(type : Type, structure : Expr) {
-        return switch type {
-            case TInst(t, _): 
-                if(t.get().meta.has(":structInit")) structure
-                else switch Context.toComplexType(type) {
-                    case TPath(p): macro new $p($structure);
-                    case _: Context.error("Invalid type for instantiation: " + type, Context.currentPos());
+    // Map an expression for a field to the real expression
+    static function mapField(from : Expr, fromType : Null<Type>, field : ObjectField) : Expr {
+        function identifier(name) return {expr: EField(from, name), pos: field.expr.pos};
+
+        return switch field.expr.expr {
+            // Lambda functions on an Array field
+            case EFunction(FArrow, func): 
+                if(func.expr == null) return Context.error("No object declaration in function", func.expr.pos);
+
+                final forVar = macro $i{func.args[0].name};
+                final functionField = switch func.args.length {
+                    case 1: field.field;
+                    case 2: func.args[1].name;
+                    case _: Context.error("Unsupported arguments in lambda function", func.expr.pos);
                 }
-            case _: structure;
+                final forIterate = identifier(functionField);
+                
+                switch func.expr.expr {
+                    case EMeta(_, {expr: EReturn(e), pos: _}): 
+                        final structureFields = objectFields(e);
+                        final structureType = switch e.expr {
+                            case ENew(typePath, _): ComplexTypeTools.toType(TPath(typePath));
+                            case _ if(fromType != null): returnTypeIfArray(returnTypeForField(fromType, field.field));
+                            case _: null;
+                        }
+
+                        //final structure = createNew(fromType, mapStructure(structureFields, forVar, structureType));
+                        final structure = mapStructure(structureFields, forVar, structureType);
+        
+                        macro [for($forVar in $forIterate) $structure];
+        
+                    case _: 
+                        Context.error("Lambda function can only contain an anonymous structure or an instantiation of a Dataclass.", func.expr.pos);
+                }
+
+            case _:
+                mapExpr(identifier(field.field), field.expr);
         }
     }
-   
-    static function mapStructure(info : DataClassInfo) : Expr {
-        final newObj = EObjectDecl(info.fields.map(f -> {
+
+    static function mapExpr(ident : Expr, e : Expr) return switch e.expr {
+
+        ///// Map Same-identifiers to Std functions /////
+
+        case EConst(CIdent("Same")):                 
+            ident;
+
+        case EConst(CIdent("SameString")):
+            macro Std.string($ident);
+
+        case EConst(CIdent("SameInt")):
+            macro Std.parseInt($ident);
+
+        case EConst(CIdent("SameFloat")):
+            macro Std.parseFloat($ident);
+
+        case EConst(CIdent("SameFloatToInt")):
+            macro Std.int($ident);
+
+        case _: 
+            e.map(mapExpr.bind(ident));
+    }
+
+    static function objectFields(e : Expr) return switch e.expr {
+        case EObjectDecl(fields) | ENew(_, [{expr: EObjectDecl(fields), pos: _}]):
+            fields;
+        case _: 
+            Context.error("Required: Anonymous object declaration or object instantiation.", e.pos);
+    }
+
+    static function mapStructure(fields : Array<ObjectField>, from : Expr, fromType : Null<Type>) : Expr {
+        final newObj = EObjectDecl(fields.map(f -> {
             field: f.field,
-            expr: mapField(info, f.field, f.expr),
+            expr: mapField(from, fromType, f),
             quotes: f.quotes
         }));
 
-        return {expr: newObj, pos: Context.currentPos()};
-    }
-
-    static function returnTypeForField(type : Type, fieldName : String) : Option<Type> {
-        function returnTypeInArray(type : Type) : Option<Type> {
-            return switch type {
-                case TLazy(f): returnTypeInArray(f());
-                case TInst(t, params) if(t.get().name == "Array"): Some(params[0]);
-                case _: None;
-            }
-        }
-    
-        return switch type {
-            case TLazy(f): 
-                returnTypeForField(f(), fieldName);
-
-            case TInst(t, _): 
-                final field = t.get().fields.get().find(f -> f.name == fieldName);
-                if(field == null) Context.error('Field not found on $type: ' + fieldName, Context.currentPos());
-                else returnTypeInArray(field.type);
-
-            case TAnonymous(a):
-                final field = a.get().fields.find(f -> f.name == fieldName);
-                if(field == null) Context.error('Field not found on $type: ' + fieldName, Context.currentPos());
-                else returnTypeInArray(field.type);
-
-            case TMono(t):
-                Context.error("No dataMap type found, please specify it explicitly.", Context.currentPos());
-
-            case _: 
-                Context.error("Unsupported type for dataMap: " + type, Context.currentPos());
-        }
-    }
-
-    // Map an expression for a field to the real expression
-    static function mapField(info : DataClassInfo, fieldName : String, e : Expr) : Expr {
-        function identifier(name) return {expr: EField(info.identifier, name), pos: e.pos};
-
-        return switch e.expr {
-            // Lambda functions on an Array field
-            case EFunction(FArrow, f): 
-                if(f.expr == null) return Context.error("No object declaration in function", e.pos);
-
-                // If no Array, ignore
-                final returnType = switch f.expr.expr {
-                    case EMeta(_, {expr: EReturn({expr: ENew(typePath, _), pos: _}), pos: _}): 
-                        ComplexTypeTools.toType(TPath(typePath));
-                    case _: 
-                        switch returnTypeForField(info.currentType, fieldName) {
-                            case Some(t): t;
-                            case None:
-                                return e.map(mapField.bind(info, fieldName));
-                        }
-                }
-
-                final functionField = switch f.args.length {
-                    case 1: fieldName;
-                    case 2: f.args[1].name;
-                    case _: Context.error("Unsupported arguments in lambda function", e.pos);
-                }
-
-                final forVar = f.args[0].name;
-                final forIterate = identifier(functionField);
-
-                final structure = createNew(returnType, mapStructure({
-                    identifier: macro $i{forVar},
-                    fields: switch f.expr.expr {
-                        case EMeta(_, {expr: EReturn({expr: EObjectDecl(fields), pos: _}), pos: _}): fields;
-                        case EMeta(_, {expr: EReturn({expr: ENew(typePath, [{expr: EObjectDecl(fields), pos: _}]), pos: _}), pos: _}): fields;
-                        case _: Context.error("Lambda function can only contain an anonymous structure or an instantiation of a Dataclass.", f.expr.pos);
-                    },
-                    currentType: returnType
-                }));
-
-                macro [for($i{forVar} in $forIterate) $structure];
-
-            case EConst(CIdent("Same")):                 
-                final ident = identifier(fieldName);
-                ident;
-
-            case EConst(CIdent("SameString")):
-                final ident = identifier(fieldName);
-                macro Std.string($ident);
-
-            case EConst(CIdent("SameInt")):
-                final ident = identifier(fieldName);
-                macro Std.parseInt($ident);
-
-            case EConst(CIdent("SameFloat")):
-                final ident = identifier(fieldName);
-                macro Std.parseFloat($ident);
-
-            case EConst(CIdent("SameFloatToInt")):
-                final ident = identifier(fieldName);
-                macro Std.int($ident);
-    
-            case _: 
-                e.map(mapField.bind(info, fieldName));
-        }
+        return createNew(fromType, {expr: newObj, pos: Context.currentPos()});
     }
 
     /**
      * The "Same" values will mess up autocompletion, so typecheck them to Any.
      * Will only happen in display mode.
      */
-    static function mapSameForDisplay(e : Expr) : Expr {
+     static function mapSameForDisplay(e : Expr) : Expr {
         return switch e.expr {
             case EConst(CIdent(s)): switch s {
                 case "Same" | "SameString" | "SameInt" | "SameFloat" | "SameFloatToInt":
@@ -173,36 +135,53 @@ class DataMap
         }
     }
 
+    static function returnTypeIfArray(type : Type) : Null<Type> {
+        return switch type {
+            case TLazy(f): returnTypeIfArray(f());
+            case TInst(t, params) if(t.get().name == "Array"): params[0];
+            case _: null;
+        }
+    }
+
+    static function returnTypeForField(type : Type, fieldName : String) : Null<Type> {   
+        return switch type {
+            case TLazy(f): 
+                returnTypeForField(f(), fieldName);
+
+            case TInst(t, _): 
+                final field = t.get().fields.get().find(f -> f.name == fieldName);
+                if(field == null) Context.error('Field not found on $type: ' + fieldName, Context.currentPos());
+                else field.type;
+
+            case TAnonymous(a):
+                final field = a.get().fields.find(f -> f.name == fieldName);
+                if(field == null) Context.error('Field not found on $type: ' + fieldName, Context.currentPos());
+                else field.type;
+
+            case _:
+                null;
+        }
+    }
+
+    static function createNew(type : Null<Type>, fromStructure : Expr) return switch type {
+        case TInst(t, _) if(!t.get().meta.has(":structInit")):
+            switch Context.toComplexType(type) {
+                case TPath(p): macro new $p($fromStructure);
+                case _: fromStructure;
+            }
+        case _: fromStructure;
+    }
+   
     #end
 
-    public static macro function dataMap(from : Expr, toStructure : Expr) {
-        if(Context.defined("display")) return mapSameForDisplay(toStructure);
+    public static macro function dataMap(from : Expr, structure : Expr) {
+        if(Context.defined("display")) return mapSameForDisplay(structure);
 
-        final expectedType = switch toStructure.expr {
+        final expectedType = switch structure.expr {
             case ENew(t, _): ComplexTypeTools.toType(TPath(t));
             case _: Context.getExpectedType();
         }
 
-        if(expectedType == null)
-            Context.error("No dataMap type found, please specify it explicitly.", toStructure.pos);
-
-        function toAnonField(e : Expr) return switch e.expr {
-            case EObjectDecl(fields): 
-                fields;
-            case ENew(typePath, [{expr: EObjectDecl(fields), pos: _}]):
-                fields;
-            case _: 
-                Context.error("Required: Anonymous object declaration or object instantiation.", e.pos);
-        }    
-    
-        final structure = mapStructure({
-            identifier: from,
-            fields: toAnonField(toStructure),
-            currentType: expectedType
-        });
-
-        //trace(structure.toString().replace("[", "\n\t[").replace("{", "\n\t{"));
-
-        return createNew(expectedType, structure);
+        return mapStructure(objectFields(structure), from, expectedType);
     }
 }
